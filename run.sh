9a0e55124578ad7c7e53b35671304ee926aaf5f2
Parham Alvani <parham.alvani@gmail.com>
2019-06-23 03:06:26 +0430

    feat: Brand new runner with Quera support in mind

--- run.sh
+++ run.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
 
-EXT=java
-LANG=java
+EXT=c
+LANG=c
 
-if [ -d out ]; then
-	echo "out folder is exist"
-	rm -Rf out
+if [ -d output ]; then
+	echo "output folder exists so remove it first"
+	rm -Rf output
 fi
-mkdir out
+mkdir output
 
-for filename in *.zip; do
-  better_name=$(echo "$filename" | sed 's, ,\ ,g')
-  echo unzip "$better_name" -d "out/$better_name"
-  unzip -q "$better_name" -d "out/$better_name"
+# loop over the student's submissions
+for student in $(ls ./input); do
+        # validate student identification
+        if [ -d ./input/$student ] && [[ $student =~ ^[0-9]+$ ]]; then
+                echo "student [$student]"
+                i=0
+                # loop over the problems
+                # [ problem name can have spaces ]
+                OIFS="$IFS"
+                IFS=$'\n'
+                for problem in $(ls ./input/$student); do
+                        i=$((i+1))
+                        printf "\t problem [$problem]\n"
+                        # unzip each problem submission
+                        for sub in $(ls ./input/$student/$problem/*.zip 2> /dev/null); do
+                                echo $sub
+                                printf "\t\t unzip $student - $problem [$i]\n"
+                                unzip -q "$sub" -d "./output/${student}_$i"
+                        done
+                done
+                IFS="$OIFS"
+        fi
 done
 
-for filename in *.rar; do
-  better_name=$(echo "$filename" | sed 's, ,\ ,g')
-  mkdir "out/$better_name"
-  echo unrar -inul x "$better_name" "out/$better_name"
-  unrar x "$better_name" "out/$better_name"
-done
-find out -name __MACOSX -type d -print0 | xargs -0 rm -r --
+# remove cache files
+find output -name __MACOSX -type d -print0 | xargs -0 rm -r --
 
-files=$(find out/ -name "*.$EXT")
-mkdir final
-
+# create the final directory put files into it with corrected filename
+OIFS="$IFS"
 IFS=$'\n'
-for filename in $files; do
-  echo $filename
-  better_name=$(echo "$filename" | sed -E  "s/([^a-zA-Z0-9\/\.])//g")
-  folder_name=$(dirname final/$better_name)
-  mkdir -p "$folder_name"
-  mv "$filename" $folder_name
+for filename in $(find output -name "*.$EXT"); do
+        better_name=$(echo "$filename" | sed -E  "s/([^a-zA-Z0-9\_\-\/\.])//g")
+        better_name=$(echo $better_name | sed -E "s/output/final/")
+        echo $better_name
+        mkdir -p $(dirname $better_name)
+        cp "$filename" "$better_name"
 done
+IFS="$OIFS"
 
-./moss.pl -l $LANG -d `find final/out -name "*.$EXT" -type f -exec grep -Iq . {} \; -and  -print`
+./moss.pl -l $LANG `find final -name "*.$EXT" -type f -exec grep -Iq . {} \; -and  -print`
+
+rm -Rf final # remove final directory
