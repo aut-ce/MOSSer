1e61e9bffa955c165fb0705e43510646ae2af6dc
Parham Alvani <parham.alvani@gmail.com>
2019-06-27 18:40:15 +0000

    feat: Print the Sorted Result on STDOUT

--- parser/cmd/root.go
+++ parser/cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sort"
 
 	"github.com/aut-ceit/mosser/parser/moss"
 	"github.com/spf13/cobra"
@@ -27,7 +28,12 @@ func Execute() {
 				fmt.Println(err)
 				return
 			}
-			fmt.Println(matches)
+
+			sort.Slice(matches, func(i, j int) bool { return matches[i].File1 < matches[j].File1 })
+			for _, match := range matches {
+				fmt.Printf("%s\t%s\t%d\n", match.File1, match.File2, match.LinesMatch)
+			}
+
 		},
 	}
 	if err := cmd.Execute(); err != nil {
